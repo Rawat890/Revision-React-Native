c3e6311120c896ea6c29e394587a10d12cf495c8
Rawat890 <rohanrawat7508@gmail.com>
2025-09-15 14:44:34 +0530

    chore: ques2 added

--- Javascript/src/Asynchronous/ques2.ts
+++ Javascript/src/Asynchronous/ques2.ts
@@ -13,9 +13,8 @@ function validateUserInput(input:any) {
   });
 }
 
-validateUserInput("a").then((data)=>console.log(data)).catch((err)=>console.log(err))
-
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
 /*Write a function that wraps XMLHttp request in Promise for making http request
 To properly wrap an XMLHttpRequest inside a Promise, you need to:
 
@@ -24,32 +23,17 @@ Attach event listeners for success (onload) and failure (onerror).
 Send the request.
 Resolve or reject the promise based on the response.*/
 
-function xmlRequest(url: string): Promise<any> {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
+//Write an async function to fetch user profile and await the response before processing
 
-    xhr.onload = () => {
-      if (xhr.status >= 200 && xhr.status < 300) {
-        try {
-          const data = JSON.parse(xhr.responseText);
-          resolve(data);
-        } catch (err) {
-          reject(new Error("Failed to parse JSON: " + err));
-        }
-      } else {
-        reject(new Error(`Request failed with status ${xhr.status}`));
-      }
-    };
+async function getUser(url:string) {
+  try {
+    const response = await fetch(url); //waits for the HTTP response,
+    const profile = await response.json(); //reads and parses the body stream as JSON
+    console.log("User Profile:", profile);
 
-    xhr.onerror = () => {
-      reject(new Error("Network error occurred"));
-    };
-
-    xhr.send();
-  });
+  } catch (error) {
+    console.log(error)
+  }
 }
+getUser(url)
 
-xmlRequest(url)
-  .then(data => console.log("Response:", data))
-  .catch(err => console.error("Error:", err));
