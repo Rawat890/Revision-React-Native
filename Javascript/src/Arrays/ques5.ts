56fff39c37a155b9764040c8cafdb9907dc38ee6
Rawat890 <rohanrawat7508@gmail.com>
2025-09-28 17:34:09 +0530

    chore: ques5 arrays updated

---
+++ Javascript/src/Arrays/ques5.ts
@@ -0,0 +1,40 @@
+function pattern1() {
+  for (let i = 0; i < 6; i++) {
+    let row = '';
+    for (let j = 0; j <= i; j++) {
+      row += '*';
+    }
+    console.log(row);
+  }
+}
+
+pattern1();
+
+function pattern2() {
+  for (let i = 6; i > 0 ; i--) {
+    let row = '';
+    for (let j = i; j > 0; j--) {
+      row += '*';
+    }
+    console.log(row);
+  }
+}
+
+pattern2();
+
+function pattern3() {
+  for (let i = 0; i <6 ; i++) {
+    let row = '';
+    for (let j = 0; j < i; j++) {
+      row += ' ';
+    }
+    for (let j = i; j < 6; j++) {
+      row += '*';
+    }
+    console.log(row);
+  }
+}
+
+console.log('\n');
+pattern3();
+
